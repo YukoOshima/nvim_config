67c2c52625b9b3c30dcb173c09c11505fd52aee4
Eason Meng <eason.meng@bytedance.com>
2023-06-16 21:56:12 +0800

    add community

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,13 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.note-taking.neorg"},
+  { import = "astrocommunity.project.project-nvim"},
+  { import = "astrocommunity.colorscheme.gruvbox-baby"}
 }
