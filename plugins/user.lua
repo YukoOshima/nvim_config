4d39e2b297c3ad8f36e26f52d9c1a2a18edc6c0a
Yusen Meng <yusenmeng@Yusens-MacBook-Pro.local>
2023-07-10 22:17:11 +0800

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,23 +1,25 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  {
-    "jcdickinson/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- }, 
+    {
+        "jcdickinson/codeium.nvim",
+        event = "BufRead",
+        commit = "b1ff0d6c993e3d87a4362d2ccd6c660f7444599f",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({
+            })
+        end
     },
-    config = function()
-      require("codeium").setup({
-      })
-    end
-  },
 }
