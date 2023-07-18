524a9d6800f67d5f9ca750c37392037ca67a2cf2
Eason Meng <eason.meng@bytedance.com>
2023-07-18 16:56:49 +0800

    Add Copilot Lua completion plugins and remove unused plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,8 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.html-css" },
